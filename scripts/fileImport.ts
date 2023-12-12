234939d30e342e5b6209c7715d9c83dc34fe8595
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2023-12-12 11:27:36 +0200

    Revert "use helper (#7)"
    This reverts commit 3768a6b843338781a32b946f22623049f70cbb18.

--- scripts/fileImport.ts
+++ scripts/fileImport.ts
@@ -1,5 +1,6 @@
-import { downloadAndExtractZip } from "@poolzfinance/poolz-helper-v2"
-import { cleanUpFolders } from "@poolzfinance/poolz-helper-v2"
+import { downloadAndExtractZip } from "../TestSetup"
+import fs from "fs"
+import path from "path"
 
 async function downloadAndExtractZipAll() {
     try {
@@ -11,11 +12,49 @@ async function downloadAndExtractZipAll() {
             "https://github.com/The-Poolz/VaultManager/archive/refs/heads/master.zip",
             "contracts/"
         )
-        await cleanUpFolders("contracts/LockDealNFT")
-        await cleanUpFolders("contracts/VaultManager")
+        cleanUpFolders("contracts/LockDealNFT")
+        cleanUpFolders("contracts/VaultManager")
     } catch (error) {
         console.error("An error occurred during download and compile:", error)
     }
+}
+
+function cleanUpFolders(parentDir: string) {
+    // Get a list of all files and subfolders in the parent directory
+    const items = fs.readdirSync(parentDir)
+
+    for (const item of items) {
+        const itemPath = path.join(parentDir, item)
+
+        // Check if it's a directory
+        if (fs.lstatSync(itemPath).isDirectory()) {
+            // If it's not named "contracts," delete it
+            if (item !== "contracts") {
+                removeFolderRecursively(itemPath)
+            }
+        } else {
+            // It's a file, so delete it
+            fs.unlinkSync(itemPath)
+        }
+    }
+}
+
+function removeFolderRecursively(folderPath: string) {
+    // Recursively delete the folder and its contents
+    const items = fs.readdirSync(folderPath)
+
+    for (const item of items) {
+        const itemPath = path.join(folderPath, item)
+
+        if (fs.lstatSync(itemPath).isDirectory()) {
+            removeFolderRecursively(itemPath)
+        } else {
+            fs.unlinkSync(itemPath)
+        }
+    }
+
+    // After all contents are deleted, remove the folder itself
+    fs.rmdirSync(folderPath)
 }
 
 downloadAndExtractZipAll()
