0e493bf0d56edc2ff055eea0721a6fc4e83f5f2e
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-12-22 12:11:38 +0200

    add ability to multiply selection

--- scripts/terminal.ts
+++ scripts/terminal.ts
@@ -1,13 +1,12 @@
 import { getMenu } from "./utility/deployment/input"
-
 import {
     deployVaultAndLockDealNFT,
     deploySimpleProviders,
     deployRefundProvider,
     deployRefundAndCollateral,
-    deployLightMigrator,
     deployBuilders,
     deployAllContracts,
+    deployLightMigrator,
     deployDelayProviderAndMigrator,
 } from "./utility/deployment/execute"
 
@@ -27,39 +26,45 @@ const menuItems = [
 ]
 
 async function displayMenu() {
-    try {
-        const answer = await getMenu(menuItems)
+    let keepMenuOpen = true
 
-        switch (answer) {
-            case menuItems[0].name:
-                await deployVaultAndLockDealNFT()
-                break
-            case menuItems[1].name:
-                await deploySimpleProviders()
-                break
-            case menuItems[2].name:
-                await deployRefundProvider()
-                break
-            case menuItems[3].name:
-                await deployRefundAndCollateral()
-                break
-            case menuItems[4].name:
-                await deployBuilders()
-                break
-            case menuItems[5].name:
-                await deployLightMigrator()
-                break
-            case menuItems[6].name:
-                await deployDelayProviderAndMigrator()
-                break
-            case "Deploy All contracts":
-                await deployAllContracts()
-                break
+    while (keepMenuOpen) {
+        try {
+            const answer = await getMenu(menuItems)
+
+            switch (answer) {
+                case "Deploy All contracts":
+                    await deployAllContracts()
+                    break
+                case menuItems[1].name:
+                    await deployVaultAndLockDealNFT()
+                    break
+                case menuItems[2].name:
+                    await deploySimpleProviders()
+                    break
+                case menuItems[3].name:
+                    await deployRefundProvider()
+                    break
+                case menuItems[4].name:
+                    await deployRefundAndCollateral()
+                    break
+                case menuItems[5].name:
+                    await deployBuilders()
+                    break
+                case menuItems[6].name:
+                    await deployLightMigrator()
+                    break
+                case menuItems[7].name:
+                    await deployDelayProviderAndMigrator()
+                    break
+                default:
+                    // Exit the loop if an invalid option is selected
+                    keepMenuOpen = false
+                    break
+            }
+        } catch (error) {
+            console.error(`Error executing command: ${error}`)
         }
-
-        console.log(`Command executed successfully: ${answer}`)
-    } catch (error) {
-        console.error(`Error executing command: ${error}`)
     }
 }
 
