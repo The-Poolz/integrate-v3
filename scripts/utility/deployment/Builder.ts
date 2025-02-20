58ac8d89cb5c06e0ea1ad60febb8d28c79af657b
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-02-20 14:43:28 +0200

    add setApprovedContracts after all cases

--- scripts/utility/deployment/Builder.ts
+++ scripts/utility/deployment/Builder.ts
@@ -1,7 +1,12 @@
+import { LockDealNFT } from "../../../typechain-types"
 import { deploy } from "../deployment"
+import { ethers } from "hardhat"
 
 export async function deployBuilder(lockDealNFT: string) {
+    const LockDealNFTFactory = await ethers.getContractFactory("LockDealNFT")
+    const LockDealNFT: LockDealNFT = LockDealNFTFactory.attach(lockDealNFT) as LockDealNFT
     await deploy("SimpleBuilder", lockDealNFT)
+    await LockDealNFT.setApprovedContract(lockDealNFT, true)
 }
 
 const lockDealNFT = process.env.LOCK_DEAL_NFT_ADDRESS || ""
--- scripts/utility/deployment/DispenserProvider.ts
+++ scripts/utility/deployment/DispenserProvider.ts
@@ -1,7 +1,12 @@
+import { DispenserProvider, LockDealNFT } from "../../../typechain-types"
 import { deploy } from "../deployment"
+import { ethers } from "hardhat"
 
 export async function deployDispenser(lockDealNFT: string) {
-    await deploy("DispenserProvider", lockDealNFT)
+    const LockDealNFTFactory = await ethers.getContractFactory("LockDealNFT")
+    const LockDealNFT: LockDealNFT = LockDealNFTFactory.attach(lockDealNFT) as LockDealNFT
+    const DispenserProviderAddress = (await deploy("DispenserProvider", lockDealNFT)) as DispenserProvider
+    await LockDealNFT.setApprovedContract(DispenserProviderAddress.address, true)
 }
 
 const lockDealNFT = process.env.LOCK_DEAL_NFT_ADDRESS || ""
