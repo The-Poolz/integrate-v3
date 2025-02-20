4f25a6467501a86b1035ebb7040e12c1b3a597c5
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-02-20 14:44:58 +0200

    fix builder address

--- scripts/utility/deployment/Builder.ts
+++ scripts/utility/deployment/Builder.ts
@@ -1,12 +1,12 @@
-import { LockDealNFT } from "../../../typechain-types"
+import { LockDealNFT, SimpleBuilder } from "../../../typechain-types"
 import { deploy } from "../deployment"
 import { ethers } from "hardhat"
 
 export async function deployBuilder(lockDealNFT: string) {
     const LockDealNFTFactory = await ethers.getContractFactory("LockDealNFT")
     const LockDealNFT: LockDealNFT = LockDealNFTFactory.attach(lockDealNFT) as LockDealNFT
-    await deploy("SimpleBuilder", lockDealNFT)
-    await LockDealNFT.setApprovedContract(lockDealNFT, true)
+    const simpleBuilder: SimpleBuilder = await deploy("SimpleBuilder", lockDealNFT)
+    await LockDealNFT.setApprovedContract(simpleBuilder.address, true)
 }
 
 const lockDealNFT = process.env.LOCK_DEAL_NFT_ADDRESS || ""
