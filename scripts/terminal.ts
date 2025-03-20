b8e8762c4a7468906e6210bc02796b90ea772639
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-03-20 11:10:03 +0200

    refactor action menu

--- scripts/terminal.ts
+++ scripts/terminal.ts
@@ -20,57 +20,42 @@ const scriptPaths = [
 ]
 
 const menuItems = [
-    { name: "Deploy All contracts" },
-    { name: "Upgrade from v1.3 to v1.4" },
-    ...scriptPaths.map((script) => ({ name: `Deploy ${script.replace(".ts", "")}` })),
-    { name: "Exit" }, // Add Exit option here
+    { name: "Deploy All contracts", action: deployAllContracts },
+    { name: "Upgrade from v1.3 to v1.4", action: upgrade },
+    ...scriptPaths.map((script, index) => ({
+        name: `Deploy ${script.replace(".ts", "")}`,
+        action: [
+            deployDispenser,
+            deployBuilder,
+            deploySimpleProviders,
+            deployVaultAndLockDealNFT,
+            deployWithoutDispenser,
+            deployInvestProvider,
+        ][index], // Map functions dynamically
+    })),
+    { name: "Exit", action: () => console.log("Exiting...") },
 ]
 
 async function displayMenu() {
     let keepMenuOpen = true
 
-    // Menu loop
     while (keepMenuOpen) {
         try {
             const answer = await getMenu(menuItems)
+            const selectedItem = menuItems.find((item) => item.name === answer)
 
-            switch (answer) {
-                case "Deploy All contracts":
-                    await deployAllContracts()
-                    break
-                case menuItems[1].name:
-                    await upgrade()
-                    break
-                case menuItems[2].name:
-                    await deployDispenser()
-                    break
-                case menuItems[3].name:
-                    await deployBuilder()
-                    break
-                case menuItems[4].name:
-                    await deploySimpleProviders()
-                    break
-                case menuItems[5].name:
-                    await deployVaultAndLockDealNFT()
-                    break
-                case menuItems[6].name:
-                    await deployWithoutDispenser()
-                    break
-                case menuItems[7].name:
-                    await deployInvestProvider()
-                    break
-                case menuItems[8].name: // Exit case
-                    console.log("Exiting...")
+            if (selectedItem) {
+                if (selectedItem.name === "Exit") {
                     keepMenuOpen = false
-                    break
-                default:
-                    console.log("Invalid option selected, exiting menu...")
-                    keepMenuOpen = false
-                    break
+                } else {
+                    await selectedItem.action()
+                }
+            } else {
+                console.log("Invalid option selected. Please try again.")
             }
         } catch (error) {
             console.error(`Error executing command: ${error}`)
-            process.exit(1) // Exit with error code in case of failure
+            process.exit(1)
         }
     }
 }
--- scripts/utility/input.ts
+++ scripts/utility/input.ts
@@ -1,42 +1,41 @@
-const { input, select } = require("@inquirer/prompts")
+import { input, select } from "@inquirer/prompts"
+
+type PromptType = "input" | "select"
 
 async function promptUser(
     message: string,
-    name: string,
-    type = "input",
+    type: PromptType = "input",
     choices: { name: string }[] = [],
     defaultValue = ""
-) {
-    let answer
+): Promise<string> {
     if (type === "input") {
-        answer = await input({ message, default: defaultValue })
-    } else if (type === "select") {
-        answer = await select({
+        return await input({ message, default: defaultValue })
+    } else {
+        return await select({
             message,
-            choices: choices.map((choice) => ({ name: choice.name, value: choice.name })),
+            choices: choices.map(({ name }) => ({ name, value: name })),
         })
     }
-    return answer
 }
 
-async function getBaseURI() {
-    return await promptUser("Enter the baseURI for NFT deployment:", "baseURI")
+async function getBaseURI(): Promise<string> {
+    return await promptUser("Enter the baseURI for NFT deployment:")
 }
 
-async function getLockDealNFTAddress() {
-    return await promptUser("Enter the LockDealNFT address for deployment:", "lockDealNFTAddress")
+async function getLockDealNFTAddress(): Promise<string> {
+    return await promptUser("Enter the LockDealNFT address for deployment:")
 }
 
-async function getDealProviderAddress() {
-    return await promptUser("Enter the current Deal Provider address for deployment:", "providerAddress")
+async function getDealProviderAddress(): Promise<string> {
+    return await promptUser("Enter the current Deal Provider address for deployment:")
 }
 
-async function getDispenserProviderAddress() {
-    return await promptUser("Enter the current Dispenser Provider address for deployment:", "providerAddress")
+async function getDispenserProviderAddress(): Promise<string> {
+    return await promptUser("Enter the current Dispenser Provider address for deployment:")
 }
 
-async function getMenu(menuItems: { name: string }[]) {
-    return await promptUser("Choose a menu item:", "menuItem", "select", menuItems)
+async function getMenu(menuItems: { name: string }[]): Promise<string> {
+    return await promptUser("Choose a menu item:", "select", menuItems)
 }
 
 export { getBaseURI, getLockDealNFTAddress, getDealProviderAddress, getDispenserProviderAddress, getMenu }
